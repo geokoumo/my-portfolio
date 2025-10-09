70645c2cea84ddeb72628d0b38b4c3cbf05170b2
Georgios Koumoundouros <gkoum11@gmail.com>
2025-10-09 17:22:22 +0300

    add CV

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <section className="hero">
       <h2>Hello, I’m Georgios 👋</h2>
       <p>Frontend Developer | React & Vue Enthusiast | Problem Solver</p>
-      <a className="btn" href="/Georgios Koumoundouros_Resume.pdf" download>
+      <a className="btn" href="/Georgios_Koumoundouros_Resume.pdf" download>
         Download CV
       </a>
     </section>
