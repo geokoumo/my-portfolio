d9b3416ea26a61c13b96ecbae87fdb569069fa56
Georgios Koumoundouros <gkoum11@gmail.com>
2025-10-09 17:11:28 +0300

    small changes to work

---
+++ src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import "./About.css";
+
+export default function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        I am a passionate Frontend Developer with experience in React and Vue.js. 
+        I love building interactive web applications that solve real problems.
+      </p>
+    </section>
+  );
+}
